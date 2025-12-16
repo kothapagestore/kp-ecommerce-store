e53789b09b3b1cbd3fc86ae7e9eee0349e1eec65
Shiva <kothapage.store98@gmail.com>
2025-12-16 20:29:59 +0530

    Fix email verification link format

--- app/(root)/auth/verify-email/[token]/page.jsx
+++ app/(root)/auth/verify-email/[token]/page.jsx
@@ -1,87 +1,67 @@
 'use client';
-
 import { Card, CardContent } from '@/components/ui/card';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { use, useEffect, useState } from 'react';
 import verifiedImg from '@/public/assets/images/verified.gif';
 import verificationFailedImg from '@/public/assets/images/verification-failed.gif';
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { WEBSITE_HOME } from '@/routes/WebsiteRoute';
-import { useSearchParams } from 'next/navigation';
-
-const EmailVerification = () => {
-  const searchParams = useSearchParams();
-  const token = searchParams.get('token');
-
-  const [status, setStatus] = useState('loading');
-
+const EmailVerification = ({ params }) => {
+  const { token } = use(params);
+  const [isVerified, setIsVerified] = useState(false);
   useEffect(() => {
-    if (!token) {
-      setStatus('failed');
-      return;
-    }
-
     const verify = async () => {
-      try {
-        const { data } = await axios.post('/api/auth/verify-email', { token });
-
-        if (data.success) {
-          setStatus('success');
-        } else {
-          setStatus('failed');
-        }
-      } catch (err) {
-        setStatus('failed');
+      const { data: verificationResponse } = await axios.post(
+        '/api/auth/verify-email',
+        { token }
+      );
+      if (verificationResponse.success) {
+        setIsVerified(true);
       }
     };
 
     verify();
   }, [token]);
-
   return (
     <Card className="w-[400px]">
-      <CardContent className="py-10">
-        {status === 'loading' && (
-          <h1 className="text-center text-lg font-semibold">
-            Verifying your email...
-          </h1>
-        )}
-
-        {status === 'success' && (
+      <CardContent>
+        {isVerified ? (
           <div>
-            <div className="flex justify-center">
+            <div className="flex justify-center items-center">
               <Image
-                src={verifiedImg}
+                src={verifiedImg.src}
+                height={verifiedImg.height}
+                width={verifiedImg.width}
                 className="h-[100px] w-auto"
                 alt="Verification success"
               />
             </div>
-            <h1 className="text-2xl font-bold text-green-500 my-5 text-center">
-              Email verification success!
-            </h1>
             <div className="text-center">
+              <h1 className="text-2xl font-bold text-green-500 my-5">
+                Email verification success!
+              </h1>
               <Button asChild>
                 <Link href={WEBSITE_HOME}>Continue Shopping</Link>
               </Button>
             </div>
           </div>
-        )}
-
-        {status === 'failed' && (
+        ) : (
           <div>
-            <div className="flex justify-center">
+            <div className="flex justify-center items-center">
               <Image
-                src={verificationFailedImg}
+                src={verificationFailedImg.src}
+                height={verificationFailedImg.height}
+                width={verificationFailedImg.width}
                 className="h-[100px] w-auto"
                 alt="Verification Failed"
               />
             </div>
-            <h1 className="text-2xl font-bold text-red-500 my-5 text-center">
-              Email verification failed!
-            </h1>
             <div className="text-center">
+              <h1 className="text-2xl font-bold text-red-500 my-5">
+                Email verification failed!
+              </h1>
               <Button asChild>
                 <Link href={WEBSITE_HOME}>Continue Shopping</Link>
               </Button>
